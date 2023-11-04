335049acb230fe55343cd5a026a9bfbf14fd6361
Srijan Shah <srijanshah09@gmail.com>
2023-11-04 23:26:14 +0530

    theme modifications and structure fixed

---
+++ lib/features/home/views/home.dart
@@ -0,0 +1,56 @@
+import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
+import 'package:flutter/material.dart';
+
+final iconList = <IconData>[
+  Icons.calendar_view_day_rounded,
+  Icons.edit_calendar_rounded,
+  Icons.add_circle_outline_rounded,
+  Icons.notifications_active_rounded,
+];
+
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  var _bottomNavIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Text App'),
+      ),
+      endDrawer: const Drawer(),
+      body: Container(
+        margin: const EdgeInsets.symmetric(
+          vertical: 20,
+          horizontal: 12,
+        ),
+        child: const Text(
+          'Flutter Demo Home Page',
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        mini: true,
+        child: const Icon(
+          Icons.play_arrow_rounded,
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+      bottomNavigationBar: AnimatedBottomNavigationBar(
+        backgroundColor : Theme.of(context).colorScheme.onSurface,
+        icons: iconList,
+        activeIndex: _bottomNavIndex,
+        gapLocation: GapLocation.end,
+        notchSmoothness: NotchSmoothness.defaultEdge,
+        onTap: (index) => setState(() => _bottomNavIndex = index),
+        //other params
+      ),
+    );
+  }
+}
---
+++ lib/utils/theme/custom_themes/icon_theme.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ZIconTheme{
+  ZIconTheme._();
+
+  static const lightIconTheme = IconThemeData(
+    color: Colors.black,
+    size: 24,
+  );
+
+  static const darkIconTheme = IconThemeData(
+    color: Colors.white,
+    size: 24,
+  );
+}
