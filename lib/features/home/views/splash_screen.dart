4ccdc9de66d9cae47fcb116d3d5c85c4ca43dc6c
Srijan Shah <srijanshah09@gmail.com>
2023-11-05 10:26:07 +0530

    go router added

---
+++ lib/features/home/views/splash_screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+@override
+  void initState() {
+    // TODO: implement initState
+    Future.delayed(Duration(seconds: 3),() => context.pushReplacementNamed('home'));
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Center(
+          child: Text(
+            'APP LOGO',
+            style: Theme.of(context).textTheme.headlineLarge,
+          ),
+        ),
+      ),
+    );
+  }
+}
